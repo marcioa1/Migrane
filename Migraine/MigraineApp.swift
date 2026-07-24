8744a630882c47237fba2109f11904eacc1aa35f
Marcio Aun Migueis <marcio@ar1mobile.com.br>
2026-07-24 16:33:35 -0300

    Remove uused Item

--- Migraine/MigraineApp.swift
+++ Migraine/MigraineApp.swift
@@ -12,7 +12,6 @@ import SwiftData
 struct MigraineApp: App {
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
-            Item.self,
             Dose.self,
         ])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
